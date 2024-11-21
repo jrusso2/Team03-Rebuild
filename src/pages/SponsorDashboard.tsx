f5d63abbfc8f1a8f79adb08efefb902f468e4021
mfschwa <124902716+mfschwa@users.noreply.github.com>
2024-11-21 06:05:41 -0500

    Update SponsorDashboard.tsx

--- src/pages/SponsorDashboard.tsx
+++ src/pages/SponsorDashboard.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import ViewDriversTable from '../ui/Sponsor/ViewDriversTable';
 const SponsorDashboard: React.FC = () => {
   return (
     <div>
       <h1>Sponsor Dashboard</h1>
+      <ViewDriversTable/>
     </div>
   );
 };
