1c2f4ac1ba9e9c513f783cf1bd3e7bb9e17658e2
mfschwa <124902716+mfschwa@users.noreply.github.com>
2024-11-21 12:52:20 -0500

    Update SponsorApplication.tsx

--- src/pages/SponsorApplication.tsx
+++ src/pages/SponsorApplication.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ViewApplicationsTable from '../ui/Sponsor/ViewApplicationsTable';
 
 const SponsorApplication: React.FC = () => {
   return (
     <div>
       <h1>Application Page</h1>
       <p>This is the application page for sponsors.</p>
+      <ViewApplicationsTable/>
     </div>
   );
 };
