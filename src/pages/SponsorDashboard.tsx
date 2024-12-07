4b6b4aa94cb096a90e0bbc51e4f5fff753380ad0
Michael Schwab <mfschwa@clemson.edu>
2024-12-07 15:30:56 -0500

    Sponsor dash: view list of drivers (can't remove them yet)

--- src/pages/SponsorDashboard.tsx
+++ src/pages/SponsorDashboard.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import ViewDriversTable from '../ui/Sponsor/ViewDriversTable';
+
 
 const SponsorDashboard: React.FC = () => {
   return (
     <div>
       <h1>Sponsor Dashboard</h1>
+      <ViewDriversTable></ViewDriversTable>
     </div>
   );
 };
