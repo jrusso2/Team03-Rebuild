4d8cd5ae83c2b0a9aaca46c79a110ed7ddc6e60a
Michael Schwab <mfschwa@clemson.edu>
2024-12-07 22:54:02 -0500

    Sponsors can see their drivers, with point scores, and can remove them.

--- src/ui/Sponsor/ViewDriversTable.tsx
+++ src/ui/Sponsor/ViewDriversTable.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
-import { useAuthenticator } from '@aws-amplify/ui-react';
+import { fetchUserAttributes } from 'aws-amplify/auth';
+import axios from 'axios';
+
 
 export type Driver = {
     driver_id: number;
@@ -11,22 +13,31 @@ export type Driver = {
 
 export default function ViewDriversTable()
 {
-    const { user } = useAuthenticator();
+    
     const [drivers, setDrivers] = useState<Driver[] | null>(null);
     const [loading, setLoading] = useState(true);
     const [error, _setError] = useState<any>(null);
-    
+    const [user_email, setEmail] = useState<string>("");
+    //let user_email:string;
 
     useEffect(() => {
     // fetching Drivers from db
-    const fetchData = async () => {
-
+    const fetchEmail = async () =>{
+        const attributes = await fetchUserAttributes();
+        let email;
+        email = await attributes['email'];
+        if(!email) return "";
+        setEmail(email);
+        return email as string;
+    };
+    const fetchData = async (email:string) => {
         //now, to fetch the drivers that belong to the sponsor
         try {
-            const response = await fetch('https://62rwb01jw8.execute-api.us-east-1.amazonaws.com/test/getDriversForSponsor?sponsorEmail=' + user?.signInDetails?.loginId);
-            const data = await response.json();
-            console.log(data.body);
-            setDrivers(JSON.parse(data.body) as Driver[]);
+            const response = await fetch('https://62rwb01jw8.execute-api.us-east-1.amazonaws.com/test/getDriversForSponsor?sponsorEmail=' + email);
+            const data = await response.json(); 
+            const parsed = JSON.parse(data.body) as Driver[];
+            //console.log()
+            setDrivers(parsed);
         } catch (error) {
             console.log(error);
         } finally {
@@ -35,7 +46,13 @@ export default function ViewDriversTable()
 
     };
 
-    fetchData();
+    const doTasks = async () => {
+        let email = await fetchEmail();
+        
+        await fetchData(email as string);
+    }
+
+    doTasks();
     }, []); // Empty dependency array means this effect runs once on mount
 
     if (loading) {
@@ -54,10 +71,32 @@ export default function ViewDriversTable()
         //return <div>{user?.signInDetails?.loginId}</div>
     }
 
+
+    const handleRemoveDriver = async (driverId: number) => {
+        
+        try {
+          {/**TODO: This should almost definitely be a POST, not a get, but I could only get a GET to work for now */}
+          const response = await axios.get(
+            `https://62rwb01jw8.execute-api.us-east-1.amazonaws.com/test/unlinkDriverFromSponsor`,
+            {
+              params: {
+                driverID: driverId,
+                sponsorEmail: user_email,
+              },
+            }
+          );
+          alert(`Removed driver with id=${driverId}`);
+          console.log(response);
+        } catch (err: any) {
+          alert(`Failed to remove driver. Error: ${err.message}`);
+        }
+      };
+
     //const drivers:Driver[] = [{fname: 'Michael', id: 1, lname:'Schwab',balance:5},{fname: 'Michael', id: 1, lname:'Schwab',balance:5},{fname: 'Michael', id: 1, lname:'Schwab',balance:5}];
     
-    
+   
     return (
+        
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <caption className="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
@@ -90,8 +129,15 @@ export default function ViewDriversTable()
                         <td className='px-6 py-4'>{driver.balance}</td>
                         <td className='px-6 py-4'></td>
                         <td className='px-6 py-4'></td>
-                        {/*TODO: Add button so that */ }
-                        <td className='px-6 py-4'>Remove</td> 
+                        {/*TODO: Add button so that Sponsor can remove a driver*/ }
+                        <td className='px-6 py-4'>
+                            <button
+                                onClick={() => handleRemoveDriver(driver.driver_id)}
+                                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                            >
+                                Remove
+                            </button>
+                        </td> 
                     </tr>
                 ))}
             </tbody>
