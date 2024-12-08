770d953edb4f061b6dab63f4465f3ab53af57939
Michael Schwab <mfschwa@clemson.edu>
2024-12-08 04:52:36 -0500

    gave sponsor ability to add remove points

--- src/ui/Sponsor/ViewDriversTable.tsx
+++ src/ui/Sponsor/ViewDriversTable.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { fetchUserAttributes } from 'aws-amplify/auth';
 import axios from 'axios';
 
+import "./Flyout.css";
 
 export type Driver = {
     driver_id: number;
@@ -18,6 +19,11 @@ export default function ViewDriversTable()
     const [loading, setLoading] = useState(true);
     const [error, _setError] = useState<any>(null);
     const [user_email, setEmail] = useState<string>("");
+    const [menuVisible, setMenuVisible] = useState<boolean>(false);
+    const [selectedDriver, setSelectedDriver] = useState<number | null>(null);
+    const [isAdding, setIsAdding] = useState<boolean>(true);
+    const [reason, setReason] = useState<string>("");
+    const [amount, setAmount] = useState<number | "">(0);
     //let user_email:string;
 
     useEffect(() => {
@@ -97,12 +103,92 @@ export default function ViewDriversTable()
         }
       };
 
+      const showMenu = () =>
+      {
+        setMenuVisible(true);
+      }
+      const hideMenu = () =>
+      {
+          setMenuVisible(false);
+      }
+
+      
+
+      const handleAddPointsHelper = (driverID:number) =>
+      {
+        setSelectedDriver(driverID);
+        setIsAdding(true);
+        showMenu();
+      }
+
+      const handleSubPointsHelper = (driverID:number) =>
+        {
+          setSelectedDriver(driverID);
+          setIsAdding(false);
+          showMenu();
+        }
+
+      const handleDeductPoints= async (subOrAdd:string) => {
+        if(!selectedDriver)
+        {
+            alert("Something went wrong. No points were removed");
+            return;
+        }
+        let driverId = selectedDriver;
+        try {
+            const response1 = await fetch('https://62rwb01jw8.execute-api.us-east-1.amazonaws.com/test/getUserIdWithEmail?email=' + user_email);
+            const data1 = await response1.json(); 
+            const parsed1 = JSON.parse(data1.body); 
+            const userID = parsed1[0]["id"];
+            
+            try
+            {
+                //const response = await fetch('' + user_email);
+                const response = await axios.get(
+                    `https://62rwb01jw8.execute-api.us-east-1.amazonaws.com/test/updateSponsorDriverPointBalance`,
+                    {
+                        params: {
+                           addOrSub: subOrAdd,
+                           amount: amount,
+                           driverId: driverId,
+                           sponsorId: userID,
+                           reason: reason,
+                      },
+                    }
+                  );
+                const data = await response; 
+                console.log(data);
+               //TODO: something more elegant to get clear something from list
+                window.location.reload();
+            }catch(err: any)
+            {
+                alert(`Failed to remove points. Error: ${err.message}`);
+            }
+
+        } catch (err: any) {
+          alert(`Failed to remove points. Error: ${err.message}`);
+        }
+      };
+
     //const drivers:Driver[] = [{fname: 'Michael', id: 1, lname:'Schwab',balance:5},{fname: 'Michael', id: 1, lname:'Schwab',balance:5},{fname: 'Michael', id: 1, lname:'Schwab',balance:5}];
     
    
     return (
         
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        
+        <div className={menuVisible ? "menu" : "nothing"} >
+            <p>How many points to {isAdding ? "add" : "deduct"}?</p>
+            <input type="number" id="amount_input" name="add_points_amount"
+                        onChange={(e) => setAmount(Number(e.target.value))} />
+            <p>Please briefly explain your reasoning for {isAdding ? "awarding" : "deducting" } points.</p>
+            <input type="text" id="reason_input" name="add_points_reason " onChange={(e) => setReason(e.target.value)}></input>
+            <p></p>
+            <button className = "add_points_submit_button" onClick={() => isAdding ? handleDeductPoints("add") : handleDeductPoints("sub")}>Submit</button>
+            <p></p>
+            <button className ="add_points_cancel_button" onClick={() => hideMenu()}>Cancel</button>
+        </div>
+
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <caption className="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
                 Your Drivers
@@ -117,14 +203,15 @@ export default function ViewDriversTable()
                         Point Balance
                     </th>
                     <th scope="col" className="px-6 py-3">
-                        ...
+                        Add
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Deduct
                     </th>
                     <th scope="col" className="px-6 py-3">
                         Remove?
                     </th>
-                    <th scope="col" className="px-6 py-3">
-                        <span className="sr-only">...</span>
-                    </th>
+                    
                 </tr>
             </thead>
             <tbody>
@@ -132,15 +219,29 @@ export default function ViewDriversTable()
                     <tr key={driver.driver_id} className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'>
                         <td className='px-6 py-4'>{driver.fname + ' ' + driver.lname}</td>
                         <td className='px-6 py-4'>{driver.balance}</td>
-                        <td className='px-6 py-4'></td>
-                        <td className='px-6 py-4'></td>
                         {/*TODO: Add button so that Sponsor can remove a driver*/ }
+                        <td className='px-6 py-4'>
+                            <button
+                                onClick={() => handleAddPointsHelper(driver.driver_id)}
+                                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                            >
+                                Give points
+                            </button>
+                        </td> 
+                        <td className='px-6 py-4'>
+                            <button
+                                onClick={() => handleSubPointsHelper(driver.driver_id)}
+                                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                            >
+                                Deduct points
+                            </button>
+                        </td> 
                         <td className='px-6 py-4'>
                             <button
                                 onClick={() => handleRemoveDriver(driver.driver_id)}
                                 className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
                             >
-                                Remove
+                                Remove Driver
                             </button>
                         </td> 
                     </tr>
